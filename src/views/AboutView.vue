c240ca05f7e51dedd39adfcc6b10bf920608b432
Alvis Lu <alvis@simpos.com.tw>
2023-05-21 13:57:32 +0800

    add rounded button

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,12 +2,26 @@
   <div class="about">
     <h1>This is an about page</h1>
     <v-btn color="indigo" @click="sayHi">say Hi!</v-btn>
+
+    <v-btn
+      @click="cancel"
+      class="text-none"
+      color="medium-emphasis"
+      min-width="92"
+      rounded
+      variant="outlined"
+      >cancel</v-btn
+    >
   </div>
 </template>
 
 <script setup>
 const sayHi = () => {
   console.log('Hi!')
+}
+
+const cancel = () => {
+  console.log('cancel!')
 }
 </script>
 
