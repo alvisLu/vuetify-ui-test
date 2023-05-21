12d1b9a0089f3be1da2f0fc10e7e4c3cb53db537
Alvis Lu <alvis@simpos.com.tw>
2023-05-21 11:57:48 +0800

    navigation bar add RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import NavigationView from './views/NavigationView.vue'
 </script>
 
 <template>
-  <header>
-    <NavigationView />
-    <div class="router">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <NavigationView />
 </template>
 
 <style scoped></style>
--- src/views/NavigationView.vue
+++ src/views/NavigationView.vue
@@ -1,64 +1,59 @@
 <template>
   <v-card>
     <v-layout>
-      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
-
       <v-app-bar color="primary" prominent>
         <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
         <v-toolbar-title>My files</v-toolbar-title>
-
         <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+        <v-btn variant="text" icon="mdi-dots-vertical" @click="dots"></v-btn>
       </v-app-bar>
 
-      <v-navigation-drawer v-model="drawer" location="bottom" temporary>
-        <v-list :items="items"></v-list>
+      <v-navigation-drawer v-model="drawer" temporary>
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :value="item"
+            color="primary"
+            :to="item.path"
+          >
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+          </v-list-item>
+        </v-list>
       </v-navigation-drawer>
 
-      <v-main style="height: 500px">
+      <v-main style="height: 100vh">
         <v-card-text>
-          The navigation drawer will appear from the bottom on smaller size screens.
+          <RouterView />
         </v-card-text>
       </v-main>
     </v-layout>
   </v-card>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    drawer: false,
-    group: null,
-    items: [
-      {
-        title: 'Foo',
-        value: 'foo'
-      },
-      {
-        title: 'Bar',
-        value: 'bar'
-      },
-      {
-        title: 'Fizz',
-        value: 'fizz'
-      },
-      {
-        title: 'Buzz',
-        value: 'buzz'
-      }
-    ]
-  }),
+<script setup>
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
 
-  watch: {
-    group() {
-      this.drawer = false
-    }
+const drawer = ref(false)
+const items = ref([
+  {
+    name: 'Home',
+    path: '/',
+    icon: 'mdi-home'
+  },
+  {
+    name: 'About',
+    path: '/about',
+    icon: 'mdi-account-multiple'
   }
+])
+
+const dots = () => {
+  console.log('我是 ...')
 }
 </script>
