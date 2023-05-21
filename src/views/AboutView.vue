da30856849a1ea43c2e9c00ccc13f6953ca5024e
Alvis Lu <alvis@simpos.com.tw>
2023-05-21 10:34:38 +0800

    add vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn color="indigo" @click="sayHi">say Hi!</v-btn>
   </div>
 </template>
+
+<script setup>
+const sayHi = () => {
+  console.log('Hi!')
+}
+</script>
 
 <style></style>
