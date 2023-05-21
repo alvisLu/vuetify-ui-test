acd9b1c99d81b8f0596d5faeb11032582a7b1291
Alvis Lu <alvis@simpos.com.tw>
2023-05-21 13:57:14 +0800

    add card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-    <h1>home page</h1>
+    <v-container class="mb-6">
+      <v-row align="start" no-gutters style="height: 150px">
+        <v-col v-for="n in 4" :key="n">
+          <v-card class="pa-2 mx-2" max-width="344" variant="outlined">
+            <v-card-item>
+              <div>
+                <div class="text-overline mb-1">OVERLINE</div>
+                <div class="text-h6 mb-1">Headline</div>
+                <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+              </div>
+            </v-card-item>
+
+            <v-card-actions>
+              <v-btn variant="outlined"> Button </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
