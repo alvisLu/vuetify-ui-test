c8282ae6673328c24016d28ba7fccfa8531dffda
Vivian Lu <vivianlu512@gmail.com>
2023-05-31 11:15:51 +0800

    add isLogin

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,17 +3,16 @@
     <v-col cols="12" sm="8" md="4">
       <v-card class="elevation-8 pt-12 pb-12 pl-6 pr-6">
         <v-card-title
-          v-text="card.cardTitle"
+          @click="isLogin ? login() : register()"
           class="font-weight-black justify-center"
           align="center"
         ></v-card-title>
-        <v-card-subtitle
-          v-text="card.cardSubTitle"
-          class="font-weight-black text-center mt-0"
-        ></v-card-subtitle>
+        {{ isLogin ? '登入' : '註冊' }}
+        <v-card-subtitle class="font-weight-black text-center mt-0"></v-card-subtitle>
         <v-form ref="form">
           <v-card-text>
             <v-text-field
+              v-if="!isLogin"
               label="輸入您的名字"
               type="text"
               class="font-weight-bold"
@@ -43,7 +42,13 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="black" class="mr-2" variant="outlined">註冊</v-btn>
+            <v-btn @click="isLogin ? login() : register()" class="mr-2">{{
+              isLogin ? '還沒有帳號嗎？立即註冊' : '已經有帳號？立即登入'
+            }}</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="black" class="mr-2" variant="outlined">{{
+              isLogin ? '登入' : '註冊'
+            }}</v-btn>
           </v-card-actions>
         </v-form>
       </v-card>
@@ -52,16 +57,9 @@
 </template>
 
 <script setup>
-const card = {
-  cardTitle: '登入',
-  cardSubTitle: '使用您 Firebase 帳戶'
-};
+import { ref } from 'vue';
+const isLogin = ref(true);
 
-const user = {
-  username: '',
-  accountId: '',
-  password: ''
-};
 const rules = {
   pswdLength: 20,
   pswdRules: [(v) => !!v || '請輸入密碼', (v) => (v && v.length <= this.pswdLength) || '最多20個字']
